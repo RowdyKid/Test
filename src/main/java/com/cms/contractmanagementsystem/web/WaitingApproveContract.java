9578404b1affd56cfbc375d865030d609ce77c0e
LWW <2641699133@qq.com>
2022-06-11 23:06:55 +0800

    待审批合同

--- src/main/java/com/cms/contractmanagementsystem/web/WaitingApproveContract.java
+++ src/main/java/com/cms/contractmanagementsystem/web/WaitingApproveContract.java
@@ -83,7 +83,7 @@ public class WaitingApproveContract extends HttpServlet {
             ArrayList<IEntity> arr = operateFlowDAO.GetEntitySet(operateFlow);
             ArrayList<Contract> contracts = new ArrayList<Contract>();
             ArrayList<Contract> contractSearch = new ArrayList<Contract>();
-            String conName = request.getParameter("conName");
+            String conName = request.getParameter("contractName");
             System.out.println(conName);
 
             Pattern pattern = Pattern.compile(conName, Pattern.CASE_INSENSITIVE);
