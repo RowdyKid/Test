2c3fe88b032f1ba5b90927222047f18d94c65e2a
LWW <2641699133@qq.com>
2022-06-12 18:03:22 +0800

    管理员搜索更新

--- src/main/java/com/cms/contractmanagementsystem/web/ad_SearchContract.java
+++ src/main/java/com/cms/contractmanagementsystem/web/ad_SearchContract.java
@@ -68,7 +68,6 @@ public class ad_SearchContract extends HttpServlet {
                 contract.SetName(new String(request.getParameter("contractName").getBytes("iso-8859-1"), "UTF-8"));
 
 
-
             }
 
 
