90bd03849f05acd7e870cc912c6f2357f0df712b
LWW <2641699133@qq.com>
2022-06-14 19:12:30 +0800

    更新

--- src/main/java/com/cms/contractmanagementsystem/web/ad_SearchContract.java
+++ src/main/java/com/cms/contractmanagementsystem/web/ad_SearchContract.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -84,48 +86,67 @@ public class ad_SearchContract extends HttpServlet {
             request.getRequestDispatcher("ad_SearchContract.jsp").forward(request, response);
 
         } else if (type.equals("search")) {
-            ContractDAO contractdao = new ContractDAO();
-            Contract contract = new Contract();
-            //查找
-            if (request.getParameter("contractName") != null) {
-                contract.SetName(request.getParameter("contractName"));
-            }
+            OperateFlowDAO operateFlowDAO = new OperateFlowDAO();
+            OperateFlow operateFlow = new OperateFlow();
 
-            ArrayList<IEntity> contracts = contractdao.GetEntitySet(contract);
-            ArrayList<String> statusCode = new ArrayList<String>();
-            for (int i = 0; i < contracts.size(); i++) {
-                Status temp = new Status();
-                temp.SetcontractNo(contracts.get(i).GetId());
-                int temp01 = ((Status) new StatusDAO().GetOneEntity(temp)).GetcontractStatus();
-                switch (temp01) {
-                    case 11:
-                        statusCode.add("起草");
-                        break;
-                    case 31:
-                        statusCode.add("会签");
-                        break;
-                    case 41:
-                        statusCode.add("定稿");
-                        break;
-                    case 51:
-                        statusCode.add("审核");
-                        break;
-                    case 61:
-                        statusCode.add("签订");
-                        break;
-                    default:
-                        statusCode.add("未知");
-                        break;
+            operateFlow.setOperateType(StatusCode.OPERATETYPE_DRAFT);
+
+            ArrayList<IEntity> arr = operateFlowDAO.GetEntitySet(operateFlow);
+            ArrayList<Contract> contracts = new ArrayList<Contract>();
+            ArrayList<Contract> contractSearch = new ArrayList<Contract>();
+            String conName = request.getParameter("contractName");
+            System.out.println(conName);
+
+            Pattern pattern = Pattern.compile(conName, Pattern.CASE_INSENSITIVE);
+
+            if (arr != null) {
+                for (int i = 0; i < arr.size(); i++) {
+                    Contract contract = (Contract) (new ContractDAO().GetOneEntity(((OperateFlow) arr.get(i)).getContractNo()));
+                    // Matcher matcher = pattern.matcher(((OperateFlow) arr.get(i)).getContent());
+                    contracts.add(contract);
                 }
+                for (int i = 0; i < contracts.size(); i++) {
+                    Matcher matcher = pattern.matcher(contracts.get(i).GetName());
+                    if (matcher.find()) {
+                        //把找到的图书放入arraySearch集合
+                        contractSearch.add(contracts.get(i));
+                    }
+                }
+
+                ArrayList<String> statusCode = new ArrayList<String>();
+                for (int i = 0; i < contractSearch.size(); i++) {
+                    Status temp = new Status();
+                    temp.SetcontractNo(contractSearch.get(i).GetId());
+                    int temp01 = ((Status) new StatusDAO().GetOneEntity(temp)).GetcontractStatus();
+                    switch (temp01) {
+                        case 11:
+                            statusCode.add("起草");
+                            break;
+                        case 31:
+                            statusCode.add("会签");
+                            break;
+                        case 41:
+                            statusCode.add("定稿");
+                            break;
+                        case 51:
+                            statusCode.add("审核");
+                            break;
+                        case 61:
+                            statusCode.add("签订");
+                            break;
+                        default:
+                            statusCode.add("未知");
+                            break;
+                    }
+                }
+
+
+                request.setAttribute("contracts", contractSearch);
+                request.setAttribute("status", statusCode);
+
+
+                request.getRequestDispatcher("ad_SearchContract.jsp").forward(request, response);
             }
 
-
-            request.setAttribute("contracts", contracts);
-            request.setAttribute("status", statusCode);
-
-
-            request.getRequestDispatcher("ad_SearchContract.jsp").forward(request, response);
         }
-
-    }
-}
+    }}
